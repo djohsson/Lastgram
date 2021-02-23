4343723083a825aa9e133ecf1687365cc89f828b
David Johansson <davidjohansson1993@gmail.com>
2021-02-23 22:21:16 +0100

    Print lastfm username instead of telegram username

--- Lastgram/Commands/NowPlayingService.cs
+++ Lastgram/Commands/NowPlayingService.cs
@@ -25,32 +25,32 @@ namespace Lastgram.Commands
 
         public async Task HandleCommandAsync(Message message, Func<Chat, string, Task> responseFunc)
         {
-            string lastFmUsername;
+            string lastfmUsername;
             List<string> parameters = message.GetParameters();
 
             if (!parameters.Any())
             {
                 // User did not provide Last.fm username. Try fetching one from the repository
-                lastFmUsername = await userRepository.TryGetUserAsync(message.From.Id);
+                lastfmUsername = await userRepository.TryGetUserAsync(message.From.Id);
 
-                if (string.IsNullOrEmpty(lastFmUsername))
+                if (string.IsNullOrEmpty(lastfmUsername))
                 {
-                    lastFmUsername = message.From.Username;
+                    lastfmUsername = message.From.Username;
 
-                    await userRepository.AddUserAsync(message.From.Id, lastFmUsername);
+                    await userRepository.AddUserAsync(message.From.Id, lastfmUsername);
                 }
             }
             else if (parameters.Count == 1)
             {
                 // User has provided a Last.fm username
-                lastFmUsername = parameters.First();
+                lastfmUsername = parameters.First();
 
-                await userRepository.AddUserAsync(message.From.Id, lastFmUsername);
+                await userRepository.AddUserAsync(message.From.Id, lastfmUsername);
             }
             else if (parameters.Count == 2 && parameters.Last().ToLowerInvariant().Equals("temp"))
             {
                 // User has provided a temporary Last.fm username
-                lastFmUsername = parameters.First();
+                lastfmUsername = parameters.First();
             }
             else
             {
@@ -58,30 +58,30 @@ namespace Lastgram.Commands
                 return;
             }
 
-            var track = await lastFmService.GetNowPlayingAsync(lastFmUsername);
+            var track = await lastFmService.GetNowPlayingAsync(lastfmUsername);
             string response;
 
             if (track.Success)
             {
                 var url = await spotifyService.TryGetLinkToTrackAsync(track.Track.ArtistName, track.Track.Name);
 
-                response = GetResponseMessage(message, track, url);
+                response = GetResponseMessage(lastfmUsername, track, url);
             }
             else
             {
-                lastFmUsername = HttpUtility.HtmlEncode(lastFmUsername);
+                lastfmUsername = HttpUtility.HtmlEncode(lastfmUsername);
 
-                response = $"Could not find <i>{lastFmUsername}</i> on last.fm";
+                response = $"Could not find <i>{lastfmUsername}</i> on last.fm";
             }
 
             await responseFunc(message.Chat, response);
         }
 
-        private static string GetResponseMessage(Message message, LastfmTrackResponse track, string url)
+        private static string GetResponseMessage(string lastfmUsername, LastfmTrackResponse track, string url)
         {
             string response;
             string artistAndName = HttpUtility.HtmlEncode($"{track.Track.ArtistName} - {track.Track.Name}");
-            string username = HttpUtility.HtmlEncode(message.From.Username);
+            string username = HttpUtility.HtmlEncode(lastfmUsername);
 
             if (!string.IsNullOrEmpty(url) && (track.Track.IsNowPlaying ?? false))
             {
